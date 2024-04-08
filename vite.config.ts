a57e650d02aa98587f618ece82e582b18ed7baa3
kotohafurukawa <kotoha.furukawa00@gmail.com>
2024-04-08 22:39:32 +0900

    新規
    tailwind/NextUIの追加
    本番デプロイ用時のパス修正

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/profile/",
   build: {
     outDir: "docs",
-    assetsDir: "profile",
+    assetsDir: "assets",
   },
   plugins: [react()],
 });
